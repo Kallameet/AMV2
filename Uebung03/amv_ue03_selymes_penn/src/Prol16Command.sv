b08b0f589b847d4dc58b996300cf0248acabc569
Reinhard <reinhard93@gmx.at>
2015-04-06 23:24:23 +0200

    abgabe

---
+++ Uebung03/amv_ue03_selymes_penn/src/Prol16Command.sv
@@ -0,0 +1,25 @@
+typedef enum 
+	{
+		Nop,
+		Loadi,
+		Jump,
+		Jumpc,
+		Jumpz,
+		Move,
+		And,
+		Or,
+		Xor,
+		Not,
+		Add,
+		Addc,
+		Sub,
+		Subc,
+		Comp,
+		Inc,
+		Dec,
+		Shl,
+		Shr,
+		Shlc,
+		Shrc,
+		Invalid
+	} Prol16Command;
