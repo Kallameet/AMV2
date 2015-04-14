298844960b4d3ac72a5ef0df0d585836662166f3
Kallameet <reinhard93@gmx.at>
2015-04-14 11:50:22 +0200

    ue5 initial commit

---
+++ Uebung05/src/sv/Prol16State.sv
@@ -0,0 +1,24 @@
+class Prol16State #(parameter int gRegs = 32);
+	pkgProl16::data_v regs[gRegs];
+	int programCounter;
+	bit cFlag;
+	bit zFlag;
+	
+	function new();
+		for (int i = 0; i < gRegs; i++) begin
+			regs[i] = '0;
+		end
+		programCounter = 0;
+		cFlag = 0;
+		zFlag = 0;
+	endfunction
+	
+	task reset();
+		for (int i = 0; i < gRegs; i++) begin
+			regs[i] = '0;
+		end
+		programCounter = 0;
+		cFlag = 0;
+		zFlag = 0;
+	endtask
+endclass
---
+++ Uebung05/src/sv/pkgProl16.sv
@@ -0,0 +1,3 @@
+package pkgProl16;
+	typedef bit [15:0] data_v;
+endpackage
